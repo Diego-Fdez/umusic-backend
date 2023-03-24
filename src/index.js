96a95667e0bcc02332ddf7178d4f7c81b7a11825
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-24 13:07:07 -0600

    add categories

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import roomsRoutes from './routes/roomsRoutes.js';
 import generateQR from './routes/codeQRRoutes.js';
+import categoriesRoutes from './routes/categoriesRoutes.js';
 
 const app = express();
 const Port = 4000;
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
@@ -38,6 +40,7 @@ app.use(cors(corsOptions));
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', roomsRoutes);
 app.use('/api/v1', generateQR);
+app.use('/api/v1', categoriesRoutes);
 
 app.listen(Port, () => {
   console.log(`Example app listening at http://localhost:${Port}`);
